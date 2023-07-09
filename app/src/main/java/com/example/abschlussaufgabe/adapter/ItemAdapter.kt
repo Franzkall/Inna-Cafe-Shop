91fe1fa78e25ed944e944eebf22c6e0b5a12801c
francescocabras <cabras181@gmail.com>
2023-07-09 21:43:06 +0200

    Abschl.Baustelle

--- app/src/main/java/com/example/abschlussaufgabe/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/abschlussaufgabe/adapter/ItemAdapter.kt
@@ -6,15 +6,12 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.abschlussaufgabe.R
-import com.example.abschlussaufgabe.R.id.image_placeholder
-import com.example.abschlussaufgabe.R.layout.item_view
-
 
 class ItemAdapter(private val itemList: List<Int>) : RecyclerView.Adapter<ItemAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val inflater = LayoutInflater.from(parent.context)
-        val itemView = inflater.inflate(item_view, parent, false)
+        val itemView = inflater.inflate(R.layout.item_view, parent, false)
         return ViewHolder(itemView)
     }
 
@@ -28,10 +25,9 @@ class ItemAdapter(private val itemList: List<Int>) : RecyclerView.Adapter<ItemAd
     }
 
     inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        private val imageView: ImageView = itemView.findViewById(image_placeholder)
+        private val imageView: ImageView = itemView.findViewById(R.id.image_placeholder)
 
         fun bind(item: Int) {
-
             imageView.setImageResource(item)
         }
     }
