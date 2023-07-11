e8a0648eba00b5255ebee013919fede460121566
francescocabras <cabras181@gmail.com>
2023-07-11 14:44:08 +0200

    Wiederestellung am 11.06.2023

--- app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
+++ app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
@@ -2,46 +2,19 @@ package com.example.abschlussaufgabe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
-import android.view.View
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bottomNavigationView: BottomNavigationView
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
-        setBottomNavigationViewVisibility(false)
-
-        val welcomeFragment = WelcomeFragment()
         val homeFragment = HomeFragment()
 
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container,welcomeFragment)
-            .add(R.id.fragment_container,homeFragment)
-            .hide(homeFragment)
+            .replace(R.id.fragment_container, homeFragment)
             .commit()
-
-        Handler(Looper.getMainLooper()).postDelayed({
-            supportFragmentManager.beginTransaction()
-                .hide(welcomeFragment)
-                .show(homeFragment)
-                .commit()
-        },1500)
-
     }
-    private fun setBottomNavigationViewVisibility(visible: Boolean) {
-        if (visible) {
-            bottomNavigationView.visibility = View.VISIBLE
-        } else {
-            bottomNavigationView.visibility = View.GONE
-        }
-    }
-
-
 }
+
+
+// Wiedererstellung//
