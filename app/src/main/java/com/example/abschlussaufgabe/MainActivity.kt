a31722ab666bd51c73930f0890cb82cf06664c32
francescocabras <cabras181@gmail.com>
2023-07-12 19:32:00 +0200

    Korrekturen am 12.07.2023( Neue Design, neue RecyclerView) am abend 19:32

--- app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
+++ app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
@@ -10,19 +10,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val images = listOf(
-            Image(R.drawable.bild1),
-            Image(R.drawable.inside),
-            Image(R.drawable.bild4),
-            Image(R.drawable.caffe),
-            Image(R.drawable.new_logo),
-            Image(R.drawable.lavazza)
-        )
-
-        val recyclerView = findViewById<RecyclerView>(R.id.imagesRecyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.setHasFixedSize(true)
-        recyclerView.adapter = ImageAdapter(this, images)
+        val casaFragment = CasaFragment()
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.cont_main, casaFragment)
+            .commit()
     }
 }
 
