e87b826617310aae8bb1a5f7562a168e2fdb405b
francescocabras <cabras181@gmail.com>
2023-07-19 11:50:19 +0200

    19.07.2022.B_N_B, implementiert

--- app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
+++ app/src/main/java/com/example/abschlussaufgabe/MainActivity.kt
@@ -3,15 +3,31 @@ package com.example.abschlussaufgabe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.abschlussaufgabe.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        navController = navHostFragment.navController
 
+        val bottomNavigationBar = binding.bottomNavigationView
+
+        setupWithNavController(bottomNavigationBar,navController)
     }
 }
 
