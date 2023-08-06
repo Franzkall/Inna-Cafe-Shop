2f76987ef171a76a9a52306a86185b53424dea25
francescocabras <cabras181@gmail.com>
2023-08-06 11:25:43 +0200

    Splash Screen.Funktioniert.

--- app/src/main/java/com/example/abschlussaufgabe/SplashActivity.kt
+++ app/src/main/java/com/example/abschlussaufgabe/SplashActivity.kt
@@ -3,20 +3,31 @@ package com.example.abschlussaufgabe
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Window
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAGS_CHANGED
 
+        )
 
-         Thread.sleep(2000)
+        Handler(Looper.getMainLooper()).postDelayed({
 
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },3000)
 
-        val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 }
+
+
 
 
