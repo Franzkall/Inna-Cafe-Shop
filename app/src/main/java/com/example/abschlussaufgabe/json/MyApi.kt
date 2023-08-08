a839d8d88df47870aac0aa348eb4d0e3ab5a9b83
francescocabras <cabras181@gmail.com>
2023-08-08 13:28:24 +0200

    Kommentare hinzugefügt

--- app/src/main/java/com/example/abschlussaufgabe/json/MyApi.kt
+++ app/src/main/java/com/example/abschlussaufgabe/json/MyApi.kt
@@ -1,11 +1,13 @@
 package com.example.abschlussaufgabe.json
 
+import com.example.abschlussaufgabe.BuildConfig
 import com.example.abschlussaufgabe.data.model.GoogleSheetResponce
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 // Diese Schnittstelle definiert eine API zum Abrufen von Getränkeinformationen.
 
@@ -23,8 +25,8 @@ private val retrofit = Retrofit.Builder()
 interface MyApiService {
 
     // Diese Methode verwendet die HTTP GET-Methode, um eine Liste von Getränken abzurufen.
-    @GET("echo?user_content_key=4S3cQ1QOR3lo2ODJNlMTNHVwk9hkd5oCqiUILRXpYvYbrEtypGNpVuGzSOPaSwck64kXqtpo98J0WRRmefCDzS3_AMx0qcnvm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnDWBRZUf39akLhqqqc71WsbIpVxFHpz6yHydJnKTQpB5p-vRjTY41fhHkCCfLxNbfthV7eEZPXvaEQqiaRbmCKFDWh8xqhi_Ig&lib=MMaZo9b1iaA3ZFg8Nd9XJ6C-41kuCiUbs")
-   suspend fun getDrinks(): GoogleSheetResponce
+    @GET("echo")
+   suspend fun getDrinks(@Query("user_content_key") apiKey: String, @Query("lib") lib: String = "MMaZo9b1iaA3ZFg8Nd9XJ6C-41kuCiUbs" ): GoogleSheetResponce
 }
 
 object MyApi {
--- app/src/main/java/com/example/abschlussaufgabe/repository/ApiRepository.kt
+++ app/src/main/java/com/example/abschlussaufgabe/repository/ApiRepository.kt
@@ -3,19 +3,20 @@ package com.example.abschlussaufgabe.repository
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.abschlussaufgabe.BuildConfig
 import com.example.abschlussaufgabe.data.model.DrinkItems
 import com.example.abschlussaufgabe.json.MyApi
 
 class ApiRepository(private val api: MyApi) {
     private val _drinks = MutableLiveData<List<DrinkItems>>()
-
+    private val apiKey = BuildConfig.API_KEY
 
     val drinks: LiveData<List<DrinkItems>>
         get() = _drinks
 
     suspend fun getDrinks() {
         try {
-            _drinks.value = api.retroFitService.getDrinks().data
+            _drinks.value = api.retroFitService.getDrinks(apiKey).data
         }catch (e: Exception) {
             Log.e("ApiRepository", "Fehler beim laden von daten: $e")
         }
