8037afc5f5be8e6f1c99d5e8111a96d15bcd8c96
francescocabras <cabras181@gmail.com>
2023-08-20 13:28:05 +0200

    barco scanner implementiert

---
+++ app/src/main/java/com/example/abschlussaufgabe/ui/RewardFragment.kt
@@ -0,0 +1,115 @@
+package com.example.abschlussaufgabe.ui
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.budiyev.android.codescanner.AutoFocusMode
+import com.budiyev.android.codescanner.CodeScanner
+import com.budiyev.android.codescanner.DecodeCallback
+import com.budiyev.android.codescanner.ErrorCallback
+import com.budiyev.android.codescanner.ScanMode
+import com.example.abschlussaufgabe.databinding.FragmentRewardBinding
+
+class RewardFragment : Fragment() {
+
+    private lateinit var binding: FragmentRewardBinding
+    private lateinit var codeScanner: CodeScanner
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRewardBinding.inflate(inflater, container, false)
+        setUpPermissions()
+        codeScanner()
+        return binding.root
+    }
+
+    private fun codeScanner() {
+        codeScanner = CodeScanner(requireContext(), binding.scannerView)
+
+        codeScanner.apply {
+            camera = CodeScanner.CAMERA_BACK
+            formats = CodeScanner.ALL_FORMATS
+
+            autoFocusMode = AutoFocusMode.SAFE
+            scanMode = ScanMode.CONTINUOUS
+            isAutoFocusEnabled = true
+            isFlashEnabled = false
+
+            decodeCallback = DecodeCallback {
+                requireActivity().runOnUiThread {
+                    binding.tvText.text = it.text
+                }
+            }
+            errorCallback = ErrorCallback {
+                requireActivity().runOnUiThread {
+                    Log.e("Main", "Camera initialization errors: ${it.message}")
+                }
+            }
+        }
+
+        binding.scannerView.setOnClickListener {
+            codeScanner.startPreview()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        codeScanner.startPreview()
+    }
+
+    override fun onPause() {
+        codeScanner.releaseResources()
+        super.onPause()
+    }
+
+    private fun setUpPermissions() {
+        val permission = ContextCompat.checkSelfPermission(
+            requireContext(),
+            android.Manifest.permission.CAMERA
+        )
+
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            makeRequest()
+        }
+    }
+
+    private fun makeRequest() {
+        requestPermissions(
+            arrayOf(android.Manifest.permission.CAMERA),
+            CAMERA_REQUEST_CODE
+        )
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when (requestCode) {
+            CAMERA_REQUEST_CODE -> {
+                if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(
+                        requireContext(),
+                        "You need the camera permission to be able to use this app!",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    // Successful
+                }
+            }
+        }
+    }
+
+    companion object {
+        private const val CAMERA_REQUEST_CODE = 101
+    }
+}
