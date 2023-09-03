ea67a717811da71c082ea32cf409413d67401f74
francescocabras <cabras181@gmail.com>
2023-09-03 14:46:15 +0200

    Layout/Design Update

--- app/src/main/java/com/example/abschlussaufgabe/repository/CasaImageRepository.kt
+++ app/src/main/java/com/example/abschlussaufgabe/repository/CasaImageRepository.kt
@@ -8,18 +8,26 @@ object CasaImageRepository {
 
     init {
         val image1 = Image(
-            R.drawable.jj5,
+            R.drawable.pk2,
             "Inna's-Cafe-Shop",
-            text =  "khjkhjhgjhjbjh ijhhu jhbh bjhhg gjhghv  jhffztgfgfftfhgfztfztf jhfhghgf jhvhghgfhgf"
+            text =  "LavAzza Kaffee-Shop-Moers - Homberger Str.5 - Tel: 02841 91660910"
         ).apply { id = 1 }
         imageList.add(image1)
     }
     init {
         val image2 = Image(
-           R.drawable.nuovo3,
-        "Lavazza Geschmack",
-            text = "mnmnmnkjj kjhjhgjhjhg mbjhghghv ggjhg"
+            R.drawable.pk5,
+        "Punkte Sammeln",
+            text = "Kaufe und sammle Punkte ganz einfach, indem du nach jeden Kauf den QR-Code scannst.Diese Punkte kannst du dann gegen Getränke oder Speisen eintauschen. Erstelle jetzt direkt in unserer App ein Konto. Viel Spaß beim Sammeln!"
         ).apply { id = 2 }
         imageList.add(image2)
     }
+    init {
+        val image3 = Image(
+            R.drawable.pk1,
+            "Traditionelle Patisserie",
+            "Genießen Sie zwischendurch unsere hausgemachten Torten und Waffeln. Erleben Sie die traditionelle italienische Patisserie begleitet mit dem unverwechselbaren Aroma von Lavazza Kaffee."
+        ).apply { id = 3 }
+        imageList.add(image3)
+   }
 }
